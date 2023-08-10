77f2c51e91a8083dd6dbb0461a16c3355b6b8a23
akaryaka <a.karyaka.2dit@gmail.com>
2023-08-10 14:39:21 +0600

    add url for images

--- constants/catalog.js
+++ constants/catalog.js
@@ -1,20 +1,20 @@
 const CATALOG = [
   {
     id: 'el1',
-    name: "FENDER PLAYER Stratocaster PF 3-Tone Sunburst",
-    img: '',
+    name: 'FENDER PLAYER Stratocaster PF 3-Tone Sunburst',
+    img: 'https://github.com/canoua/guitar-shop/blob/master/img/1.jpeg',
     price: 137000,
   },
   {
     id: 'el2',
-    name: "FENDER PLAYER Stratocaster PF Polar White",
-    img: '',
+    name: 'FENDER PLAYER Stratocaster PF Polar White',
+    img: 'https://github.com/canoua/guitar-shop/blob/master/img/2.jpg',
     price: 97270,
   },
   {
-    id: 'FENDER PLAYER Stratocaster MN Black',
-    name: "",
-    img: '',
+    id: 'el3',
+    name: 'FENDER PLAYER Stratocaster MN Black',
+    img: 'https://github.com/canoua/guitar-shop/blob/master/img/3.jpg',
     price: 97500,
   }
 ]
