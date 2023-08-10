57bca7a59858770e9b2adafe384a841faaa00f02
akaryaka <a.karyaka.2dit@gmail.com>
2023-08-10 14:35:19 +0600

    add images

---
+++ constants/catalog.js
@@ -0,0 +1,20 @@
+const CATALOG = [
+  {
+    id: 'el1',
+    name: "FENDER PLAYER Stratocaster PF 3-Tone Sunburst",
+    img: '',
+    price: 137000,
+  },
+  {
+    id: 'el2',
+    name: "FENDER PLAYER Stratocaster PF Polar White",
+    img: '',
+    price: 97270,
+  },
+  {
+    id: 'FENDER PLAYER Stratocaster MN Black',
+    name: "",
+    img: '',
+    price: 97500,
+  }
+]
