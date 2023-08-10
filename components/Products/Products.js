7cb70ffb5f2e0bf2819397bfa024d3841a014c02
akaryaka <a.karyaka.2dit@gmail.com>
2023-08-10 15:42:30 +0600

    Products.js

---
+++ components/Products/Products.js
@@ -0,0 +1,29 @@
+class Products {
+  //метод, который отображает данные на странице
+  //берем файл catalog.js, перебираем его и отображаем его на странице в виде html-кода
+  //деструктуризация - выводим как отдельные переменные
+  render() {
+    let htmlCatalog = '';
+    CATALOG.forEach(({id, name, price, img}) => {
+      // console.log(id, name, price, img);
+      htmlCatalog += `
+        <li>
+          <span>${name}</span>  
+          <img src='${img}' />
+          <span>${price}</span>
+          <button>Добавить в корзину</button>
+        </li>      
+      `;
+
+      const html = `
+        <ul>
+          ${htmlCatalog}
+        </ul>
+      `;
+    })
+  }
+}
+
+let productsPage= new Products();
+
+productsPage.render();
--- constants/catalog.js
+++ constants/catalog.js
@@ -2,6 +2,7 @@ const CATALOG = [
   {
     id: 'el1',
     name: 'FENDER PLAYER Stratocaster PF 3-Tone Sunburst',
+    //ссылка на гитхаб, чтобы делать запрос
     img: 'https://github.com/canoua/guitar-shop/blob/master/img/1.jpeg',
     price: 137000,
   },
