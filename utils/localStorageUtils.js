e94e8f2af5e0558b44a60b0267a3223ae1d8eedd
akaryaka <a.karyaka.2dit@gmail.com>
2023-08-11 15:14:17 +0600

    add localstorage

---
+++ utils/localStorageUtils.js
@@ -0,0 +1,41 @@
+class LocalStorageUtil{
+  constructor() {
+    this.keyName = 'products';
+  }
+
+  getProducts() {
+    const productsLocalStorage = localStorage.getItem(this.keyName);
+    //если есть какое-то значение есть в localstorage, то вернется строка, иначе - null
+    if(productsLocalStorage !== null) {
+      //если не равно null, то нужно распарсить строку, которая вернется, и перевести ее массив
+      return JSON.parse(productsLocalStorage);
+    }
+  }
+
+  putProducts(id) {
+    let products = this.getProducts();
+    //для проверки - добавлен элемент или нет
+    let pushProduct = false;
+    const index = products.indexOf(id);
+    
+    //проверка ну дубли в массиве
+    if(index === -1) {
+      products.push(id);
+      pushProduct = true;
+    } else {
+      //???
+      products.splice(index, 1);
+    }
+
+    localStorage.setItem(this.keyName, JSON.stringify(products));
+
+    return{ pushProduct, products } 
+  }
+}
+
+const localStorageUtil = new LocalStorageUtil();
+
+localStorageUtil.putProducts('el2');
+// localStorageUtil.putProducts('el3');
+
+// console.log(a);
